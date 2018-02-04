420f170745e29d6407681dbf951067fe9b58144a
AL <andylubin@gmail.com>
2018-02-04 00:32:33 -0800

    Added Authentication to the UI
    https://css-tricks.com/firebase-react-part-2-user-authentication/

--- src/firebase.js
+++ src/firebase.js
@@ -9,4 +9,6 @@ const config = {
     messagingSenderId: "352968555760"
 };
 firebase.initializeApp(config);
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
 export default firebase;
